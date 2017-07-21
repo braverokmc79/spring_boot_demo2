5c2aefbe6e03279b84e18e93062673ccd2f78569
NoteBook-Junho-Choi <braverokmc79@gmail.com>
2017-07-21 19:09:07 +0900

    Exercise-1. Mustache Template Engine을 효과적으로 학습하는 방법 - Mustache 템플릿 문법

--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -1,5 +1,8 @@
 package net.slipp.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +18,11 @@ public class WelcomeController {
 
 	@GetMapping("/helloworld")
 	public String welcome(String name, int age,  Model model) {
+		List<MyModel> repo =Arrays.asList(new MyModel("javajigi"),
+				new MyModel("kete77"));
+		
 		System.out.println("name : " +name + " age : "+ age);
+		model.addAttribute("repo",repo);
 		model.addAttribute("name", name);
 		model.addAttribute("age" , age);
 		return "welcome";
@@ -24,7 +31,5 @@ public class WelcomeController {
 	
 	
 }
-
-
 
 
